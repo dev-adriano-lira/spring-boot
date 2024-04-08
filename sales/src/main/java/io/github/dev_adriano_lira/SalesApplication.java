82fa89ca23de401f853efb6db337073c301399a7
AdrianoLira <adriano.lira@cyan-agro.com>
2024-04-08 12:06:26 -0300

    Hello World!

--- sales/src/main/java/io/github/dev_adriano_lira/SalesApplication.java
+++ sales/src/main/java/io/github/dev_adriano_lira/SalesApplication.java
@@ -3,9 +3,18 @@ package io.github.dev_adriano_lira;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SalesApplication {
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+
     public static void main(String[] args) {
        SpringApplication.run(SalesApplication.class, args);
     }
